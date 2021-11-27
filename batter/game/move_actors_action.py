3d9fcfb3dc00bda8c8f66f58b0581977d1f5dd35
courtneymickelsen <lam15020@byui.edu>
2021-11-27 09:48:51 -0700

    yee

--- batter/game/move_actors_action.py
+++ batter/game/move_actors_action.py
@@ -2,9 +2,13 @@ from game.action import Action
 from game.point import Point
 
 class MoveActorsAction(Action):
-    
-    def execute(self, cast):
-        for group in cast:
-            
-            position = cast[group].get_velocity() + cast[group].get_position()
-            cast[group]._set_position(position)
+    def __init__(self):
+        pass
+
+    # def execute(self, cast):
+    #     for group in cast:
+    #         for actor in group:
+    #             # HELP: I can't access the actual actors in the dictionary
+    #             # They're in the dictionary as storage locations
+    #             new_position = cast[group][actor].get_velocity() + cast[group][actor].get_position()
+    #             cast[group][actor]._set_position(new_position)
