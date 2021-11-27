7b68efcc52c874e7af478cd23cf2de76b30cf266
courtneymickelsen <lam15020@byui.edu>
2021-11-27 10:55:34 -0700

    i think it's finally workinggg

--- batter/game/control_actors_action.py
+++ batter/game/control_actors_action.py
@@ -1,4 +1,11 @@
 from game.action import Action
+from game.input_service import InputService
 
 class ControlActorsAction(Action):
-    pass
+    def __init__(self):
+        pass
+
+    def execute(self):
+        direction = InputService.get_direction()
+        # finish this!
+        print(direction)
